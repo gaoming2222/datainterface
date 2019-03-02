09ef0d7e748590adf53fb7dff4cf4cf6b6425cc1
pure <pure@DESKTOP-3ED72H1>
2019-03-02 15:01:24 +0800

    test

--- src/main/java/com/itclj/database/common/CommonConstant.java
+++ src/main/java/com/itclj/database/common/CommonConstant.java
@@ -3,6 +3,6 @@ package com.itclj.database.common;
 import java.text.SimpleDateFormat;
 
 public class CommonConstant {
-	//字符串与时间格式的转换  ddd
+	//字符串与时间格式的转换  dddggg
 	public static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss"); 
 }
